b577859f08d99d620dd16221c844f1c8fc421633
likaf <l1kaf@yandex.ru>
2023-10-29 21:11:03 +0300

    add php_codesniffer, update Makefile

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greering() 
+function greering()
 {
     line("Welcome to the Brain Game!");
     $name = prompt("May I have your name?");
